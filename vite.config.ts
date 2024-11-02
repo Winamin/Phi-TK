79234e33f622d1d61de87c6108d4fb1eb5cc6e91
Beks <yingxuw814@gmail.com>
2024-11-02 18:15:41 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,10 @@ import { resolve, dirname } from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  // Load environment variables
+  const env = loadEnv(mode, process.cwd());
+  process.env = { ...process.env, ...env };
+
   return {
     plugins: [
       vue(),
@@ -21,6 +24,18 @@ export default defineConfig(({ mode }) => {
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+    build: {
+      chunkSizeWarningLimit: 1000,
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return id.toString().split('node_modules/')[1].split('/')[0].toString();
+            }
+          },
+        },
       },
     },
   };
