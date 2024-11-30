f116832a0ff2c53f203b77acf8878ff4af5f0d53
Beks <yingxuw814@gmail.com>
2024-11-30 23:18:23 +0800

    Update common.ts

--- src/common.ts
+++ src/common.ts
@@ -29,7 +29,7 @@ export function isNumeric(num: any) {
 }
 
 export function setTitle(title: string) {
-  document.title = title.length ? title + ' - Phira' : 'Phira';
+  document.title = title.length ? title + ' - Phigros' : 'Phigros';
 }
 
 export function changeLocale(locale: string) {
