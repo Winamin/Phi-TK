b0a3946d19a8e4860e5eb12b31745221fd9dd815
Link <yingxuw814@gmail.com>
2024-12-07 20:10:52 +0800

    Update render.rs

--- src-tauri/src/render.rs
+++ src-tauri/src/render.rs
@@ -83,6 +83,7 @@ impl RenderConfig {
             volume_sfx: self.volume_sfx,
             chart_debug: self.chart_debug,
             chart_ratio: self.chart_ratio,
+            buffer_size: self.buffer_size,
             ..Default::default()
         }
     }
