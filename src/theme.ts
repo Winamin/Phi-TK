de2ea349004ca3f32ef065e69a77df4e31d28ef1
Link <yingxuw814@gmail.com>
2024-12-22 12:24:22 +0800

    window.matchMedia

--- src/theme.ts
+++ src/theme.ts
@@ -1,10 +1,39 @@
 // Generated using https://theme-generator.vuetifyjs.com/
 
+import { useVuetify } from 'vuetify/lib/framework';
+import { ref, watch } from 'vue';
+
 export default {
-  dark: true,
-  colors: {
-    primary: '#e91e63',
-    secondary: '#f44336',
-    accent: '#e91e63',
+  setup() {
+    const vuetify = useVuetify();
+    const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const isDarkMode = ref(prefersDarkMode);
+
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
+      isDarkMode.value = e.matches;
+      vuetify.framework.theme.dark = isDarkMode.value;
+    });
+
+    vuetify.framework.theme.dark = isDarkMode.value;
+
+    return {
+      isDarkMode,
+    };
+  },
+
+  theme: {
+    dark: true,
+    themes: {
+      light: {
+        primary: '#e91e63',
+        secondary: '#f44336',
+        accent: '#e91e63',
+      },
+      dark: {
+        primary: '#e91e63',
+        secondary: '#f44336',
+        accent: '#e91e63',
+      },
+    },
   },
 };
