c4f39851f519f0de71f4ff90761daac8b5fd7213
Link <yingxuw814@gmail.com>
2024-12-22 23:54:05 +0800

    Update theme.ts

--- src/theme.ts
+++ src/theme.ts
@@ -1,39 +1,40 @@
 // Generated using https://theme-generator.vuetifyjs.com/
 
-import { useVuetify } from 'vuetify/lib/framework';
-import { ref, watch } from 'vue';
+import { createVuetify } from 'vuetify';
+import 'vuetify/styles';
+import { aliases, mdi } from 'vuetify/iconsets/mdi';
 
-export default {
-  setup() {
-    const vuetify = useVuetify();
-    const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    const isDarkMode = ref(prefersDarkMode);
-
-    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
-      isDarkMode.value = e.matches;
-      vuetify.framework.theme.dark = isDarkMode.value;
-    });
-
-    vuetify.framework.theme.dark = isDarkMode.value;
-
-    return {
-      isDarkMode,
-    };
-  },
-
-  theme: {
-    dark: true,
-    themes: {
-      light: {
-        primary: '#e91e63',
-        secondary: '#f44336',
-        accent: '#e91e63',
-      },
-      dark: {
-        primary: '#e91e63',
-        secondary: '#f44336',
-        accent: '#e91e63',
-      },
-    },
+const myCustomLightTheme = {
+  dark: false,
+  colors: {
+    primary: '#e91e63',
+    secondary: '#f44336',
+    accent: '#e91e63',
   },
 };
+
+const myCustomDarkTheme = {
+  dark: true,
+  colors: {
+    primary: '#e91e63',
+    secondary: '#f44336',
+    accent: '#e91e63',
+  },
+};
+
+const vuetify = createVuetify({
+  theme: {
+    defaultTheme: 'myCustomLightTheme',
+    themes: {
+      myCustomLightTheme,
+      myCustomDarkTheme,
+    },
+  },
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: { mdi },
+  },
+});
+
+export default vuetify;
