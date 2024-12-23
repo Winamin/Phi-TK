fce76945e2e8f642998886168bb5029e6abeaeea
Link <yingxuw814@gmail.com>
2024-12-23 22:45:19 +0800

    Update theme.ts

--- src/theme.ts
+++ src/theme.ts
@@ -1,46 +1,12 @@
 // Generated using https://theme-generator.vuetifyjs.com/
 
-import { createVuetify } from 'vuetify';
-import 'vuetify/styles';
-import { aliases, mdi } from 'vuetify/iconsets/mdi';
-import { watchEffect } from 'vue';
+// Generated using https://theme-generator.vuetifyjs.com/
 
-watchEffect(() => {
-  const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-  vuetify.framework.theme.global.name.value = prefersDarkMode ? 'myCustomDarkTheme' : 'myCustomLightTheme';
-});
-
-const myCustomLightTheme = {
-  dark: false,
-  colors: {
-    primary: '#e91e63',
-    secondary: '#f44336',
-    accent: '#e91e63',
-  },
-};
-
-const myCustomDarkTheme = {
+export default {
   dark: true,
   colors: {
-    primary: '#e91e63',
+    primary: '#3f51b5',
     secondary: '#f44336',
     accent: '#e91e63',
   },
 };
-
-const vuetify = createVuetify({
-  theme: {
-    defaultTheme: 'myCustomLightTheme',
-    themes: {
-      myCustomLightTheme,
-      myCustomDarkTheme,
-    },
-  },
-  icons: {
-    defaultSet: 'mdi',
-    aliases,
-    sets: { mdi },
-  },
-});
-
-export default vuetify;
