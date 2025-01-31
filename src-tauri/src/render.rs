fe4b8f16fa8531bf78db8a295ef544cdfdab6136
Link <yingxuw814@gmail.com>
2025-01-31 20:43:23 +0800

    Modify: Loading &config

--- src-tauri/src/render.rs
+++ src-tauri/src/render.rs
@@ -334,7 +334,7 @@ pub async fn main() -> Result<()> {
     let config_ref = &config;
     let mut main = Main::new(
         Box::new(
-            LoadingScene::new(GameMode::Normal, info, config_ref.clone(), fs, Some(player), None, None).await?,
+            LoadingScene::new(GameMode::Normal, info, &config_ref.clone(), fs, Some(player), None, None).await?,
         ),
         tm,
         {
