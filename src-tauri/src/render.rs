4cd94a7f5fc442cae77969cef1620fb5d6ef839b
Link <yingxuw814@gmail.com>
2025-02-03 05:31:16 +0800

    Update render.rs

--- src-tauri/src/render.rs
+++ src-tauri/src/render.rs
@@ -338,6 +338,7 @@ pub async fn main() -> Result<()> {
     let mut writer = BufWriter::new(input);
     for sample in output.into_iter() {
         writer.write_all(&sample.to_le_bytes()
+    }
     drop(writer);
     proc.wait()?;
 
