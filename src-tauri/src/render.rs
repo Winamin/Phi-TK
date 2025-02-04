ba5b430c98b5bb7bb936c1e12ebe9aeeda0f641a
Link <yingxuw814@gmail.com>
2025-02-04 22:31:09 +0800

    Update render.rs

--- src-tauri/src/render.rs
+++ src-tauri/src/render.rs
@@ -217,20 +217,21 @@ pub async fn main() -> Result<()> {
     let mut gl = unsafe { get_internal_gl() };
 
     let volume_music = std::mem::take(&mut config.volume_music);
-    let volume_sfx =ength + A + params.config.ending_length;
-    let offset = chart.offset.m std::mem::take(&mut config.volume_sfx);
+    let volume_sfx = std::mem::take(&mut config.volume_sfx);
+
     let O: f64 = if params.config.disable_loading {
         GameScene::BEFORE_TIME as f64
     } else {
         LoadingScene::TOTAL_TIME as f64 + GameScene::BEFORE_TIME as f64
     };
     let A: f64 = -0.5; // fade out time
+    let musica: f64 = 0.7 + 0.3 + EndingScene::BPM_WAIT_TIME;
 
     let length = track_length - chart.offset.min(0.) as f64 + 1.;
     let video_length = O + length + A + params.config.ending_length;
     let offset = chart.offset.max(0.);
-    let render_start_time = Instant::now();
 
+    let render_start_time = Instant::now();
     send(IPCEvent::StartMixing);
     let mixing_output = NamedTempFile::new()?;
     let sample_rate = 96000;
