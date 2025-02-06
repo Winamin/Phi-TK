078136fb5999edcd0659e7b35f94dd98fb5a89ad
Link <yingxuw814@gmail.com>
2025-02-06 22:14:09 +0800

    Update RenderView.vue

--- src/RenderView.vue
+++ src/RenderView.vue
@@ -1,6 +1,6 @@
 <i18n>
 en:
-  already-running: Phigros TK is already running
+  already-running: Phi TK is already running
 
   prev-step: Previous
   next-step: Next
@@ -46,7 +46,7 @@ en:
   ffmpeg-not-found: You haven't installed ffmpeg yet. Please download FFmpeg.exe and put it in the specific folder.
 
 zh-CN:
-  already-running: Phigros TK 已经在运行
+  already-running: Phi TK 已经在运行
 
   prev-step: 上一步
   next-step: 下一步
