2e6b752498155c94f81a139c91c2118a1b243a3b
Link <yingxuw814@gmail.com>
2025-03-01 13:09:28 +0800

    fxaa

--- src/components/ConfigView.vue
+++ src/components/ConfigView.vue
@@ -547,6 +547,7 @@ async function replacePreset() {
           <v-text-field :label="t('fps')" type="number" :rules="[RULES.positiveInt]" density="compact" variant="outlined" v-model="fps"/>
           <v-combobox :label="t('bitrateControl')" :items="['CRF', 'CBR']" density="compact" variant="outlined" v-model="bitrateControl"/>
           <v-text-field :label="t('bitrate')" type="number" :rules="[RULES.positiveInt]" density="compact" variant="outlined" v-model="bitrate"/>
+          <v-text-field :label="t('fxaa')" type="number" :rules="[RULES.positiveInt]" density="compact" variant="outlined" v-model="fxaa"/>
         </div>
         
         <div class="switch-group">
