e5b847cbd9e0c90c564e20c99aba64622411ab41
Link <yingxuw814@gmail.com>
2025-03-28 19:43:17 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -13,8 +13,12 @@ import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 import { aliases, mdi } from 'vuetify/iconsets/mdi';
-import { VStepper, VStepperActions, VStepperHeader, VStepperItem } from 'vuetify/labs/components';
-
+import {
+  VStepper,
+  VStepperActions,
+  VStepperHeader,
+  VStepperItem
+} from 'vuetify/labs/components'
 import 'vuetify/styles';
 
 import theme from './theme';
